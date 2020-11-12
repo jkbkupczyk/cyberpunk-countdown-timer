e0084549f5cf3c83c8ac56b9452d585db498bd9b
Jakub Kupczyk <69753899+jkbkupczyk@users.noreply.github.com>
2020-11-12 13:43:59 +0100

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+// DOM
+const daysElem = document.querySelector("#daysElem");
+const hoursElem = document.querySelector("#hoursElem");
+const minutesElem = document.querySelector("#minutesElem");
+const secondsElem = document.querySelector("#secondsElem");
+
+
+const release = "10 Dec 2020";
+
+function validate(arg){
+    return arg < 10 ? `0${arg}` : arg;
+}
+
+function calcDate(){
+    let releaseDate = new Date(release);
+    let currentDate = new Date();
+
+    const date = releaseDate - currentDate;
+
+    const days = Math.floor(date / (1000 * 3600 * 24));
+    const hours = Math.floor(date / (1000 * 3600) % 12);
+    const minutes = Math.floor(date / (1000 * 60) % 60);
+    const seconds = Math.floor(date / 1000 % 60);
+
+    
+    daysElem.innerHTML = days;
+    hoursElem.innerHTML = validate(hours);
+    minutesElem.innerHTML = validate(minutes);
+    secondsElem.innerHTML = validate(seconds);
+}
+
+calcDate();
+
+setInterval(calcDate, 1000);
